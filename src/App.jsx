572cd382797d47e68e1cd63686f9f304df8d40a2
Jaiten <Jaitensahu9@gmail.com>
2024-03-01 00:45:46 +0530

    ad

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,47 @@ import "./App.css";
 function App() {
   let link = useRef();
 
-  function postLongUrl() {
-    console.log(link.current.value);
-    try {
-      fetch("http://localhost:5173/url-shortner", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ "url": link.current.value }),
-      }).then(response => {
-        if (!response.ok) {
-          throw new Error("Failed to post URL to the backend");
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log("URL successfully posted to the backend", data);
-      })
-      .catch(error => {
-        console.error("Error posting URL to the backend:", error);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // function postLongUrl() {
+  //   console.log(link.current.value);
+  //   try {
+  //     fetch("http://localhost:5173/url-shortner", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ "url": link.current.value }),
+  //     }).then(response => {
+  //       if (!response.ok) {
+  //         throw new Error("Failed to post URL to the backend");
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(data => {
+  //       console.log("URL successfully posted to the backend", data);
+  //     })
+  //     .catch(error => {
+  //       console.error("Error posting URL to the backend:", error);
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   return (
     <>
-      <h1 className="bg-red-500">URL SHORTNER</h1>
-      {/* <form action="http://localhost:5000/url-shortner" method="post"> */}
-      <input type="text" placeholder="Enter Url" ref={link} />
-      <button onClick={() => postLongUrl()}>Submit</button>
-      {/* </form> */}
+      <h1 className="bg-red-500 text-2xl">URL SHORTNER</h1>
+      <form action="http://localhost:5000/url-shortner/" method="post">
+        <input
+          type="text"
+          placeholder="Enter Url"
+          id="longUrl"
+          name="longUrl"
+          className="border border-black w-[100%] py-1 px-3 max-w-[800px] mt-3"
+        />
+        <button className="bg-green-600 px-5 py-1 font-bold ml-2 text-white">
+          Submit
+        </button>
+      </form>
     </>
   );
 }
